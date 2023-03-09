3dc4da05da52aec0564899b49e67c8a8319c3d9c
Albin Larsson <albin04.larsson@gmail.com>
2023-03-09 18:32:40 +0100

    worked

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { readFile } from 'fs/promises';
 import * as cheerio from 'cheerio';
-import { copyFileSync } from 'fs';
 
-let sum
+let sum = 0;
 const output = [];
 const match = {
     "A":20, "B":17.5, "C":15, "D":12.5, "E":10, "F":0
@@ -18,20 +17,15 @@ async function getData() {
             }
         }
     });
-    /*
-    for (let i = 0; i < output.length; i++) {
-        console.log(output[i])
-    }
-    */
     test()
 }
 
 function test() {
     for (let i = 0; i < output.length; i++) {
         if (output[i].substring(3, 4) == "p" || output[i].substring(2, 3) == "p") {
-            if (isNaN(Number(output[i+1]))) {
-                sum = match[output[i+1].substring(0,1)] * Number(output[i].substring(0,output[i].length-1));
-            }
+            if (isNaN(output[i+1].substring(0,1))) {
+                sum += match[output[i+1].substring(0,1)] * Number(output[i].substring(0,output[i].length-1));
+            } else {}
         }
     }
     console.log(sum)
